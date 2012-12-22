1c854a8e5bb952303133a61d5366121e3e5f1601
paul-home <pzaloznyj@gmail.com>
2012-12-23 00:28:05 +0200

    created basic api

---
+++ ClickrAPI/HtmlHelper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using HtmlAgilityPack;
+
+namespace ClickrAPI
+{
+    public class HtmlHelper
+    {
+        private const string address = "http://www.dota2.com/heroes/";
+        public HtmlDocument document { get; set; }
+
+        public HtmlHelper()
+        {
+            document = new HtmlDocument();
+            document.LoadHtml(new WebClient().DownloadString(address));
+        }
+
+        public IEnumerable<HtmlNode> GetHeroesAttributes()
+        {
+            return document.DocumentNode.Descendants("a")
+                           .SelectMany(a => a.Attributes.Where(b => b.Value.Contains("link_")).Select(b => b.OwnerNode));
+        }
+
+        public IEnumerable<string> GetHeroesNames()
+        {
+            return document.DocumentNode.Descendants("a")
+                           .SelectMany(a => a.Attributes.Where(b => b.Value.Contains("link_"))
+                                             .Select(c => c.Value.Split('_')[1].ToUpperInvariant()));
+        }
+    }
+}
---
+++ ClickrAPITests/HtmlHelperTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using ClickrAPI;
+using FluentAssertions;
+using HtmlAgilityPack;
+using Xunit;
+using System.Linq;
+
+namespace ClickrAPITests
+{
+    public class HtmlHelperTests
+    {
+        [Fact]
+        public void should_get_list_of_heroes()
+        {
+            //arrange
+            var helper = new HtmlHelper();
+            //act
+            var result = helper.GetHeroesNames();
+            //assert
+            result.ToList().Should().Contain("TINY");
+        }
+
+        [Fact]
+        public void should_get_heroes_attributes()
+        {
+            //arrange
+            var helper = new HtmlHelper();
+            //act
+            var result = helper.GetHeroesAttributes();
+            //assert
+            result.ToList().ForEach(a => a.Attributes.ToList().ForEach(b => Console.WriteLine(b.Name + " -> " + b.Value)));
+
+            result.ToList().Should().NotBeEmpty();
+        }
+    }
+}
